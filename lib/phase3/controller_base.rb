b4743a0191f4bdd90c1f03e05cb8b13fb80fdfa1
dcurletti <curlettid@gmail.com>
2015-01-13 17:59:11 -0800

    day 1, final commit

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -1,12 +1,19 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+    	controller_name = self.class.name.underscore
+
+    	contents = File.read("views/#{controller_name}/#{template_name}.html.erb")
+
+    	render_content(ERB.new(contents).result(binding), 'text/html')
     end
   end
 end
+
--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -1,46 +1,62 @@
+require 'byebug'
 module Phase6
   class Route
-    attr_reader :pattern, :http_method, :controller_class, :action_name
+	attr_reader :pattern, :http_method, :controller_class, :action_name
 
-    def initialize(pattern, http_method, controller_class, action_name)
-    end
+	def initialize(pattern, http_method, controller_class, action_name)
+		@pattern = pattern
+		@http_method = http_method
+		@controller_class = controller_class
+		@action_name = action_name
+	end
 
-    # checks if pattern matches path and method matches request method
-    def matches?(req)
-    end
+	# checks if pattern matches path and method matches request method
+	def matches?(req)
+		!!req.path.match(@pattern) && req.request_method == @http_method.downcase.to_sym
+	end
 
-    # use pattern to pull out route params (save for later?)
-    # instantiate controller and call controller action
-    def run(req, res)
-    end
+	# use pattern to pull out route params (save for later?)
+	# instantiate controller and call controller action
+	def run(req, res)
+	end
   end
 
   class Router
-    attr_reader :routes
+	attr_reader :routes
 
-    def initialize
-    end
+	def initialize
+		@routes = []
+	end
 
-    # simply adds a new route to the list of routes
-    def add_route(pattern, method, controller_class, action_name)
-    end
+	# simply adds a new route to the list of routes
+	def add_route(pattern, method, controller_class, action_name)
+		new_route = Route.new(pattern, method, controller_class, action_name)
+		@routes << new_route
+	end
 
-    # evaluate the proc in the context of the instance
-    # for syntactic sugar :)
-    def draw(&proc)
-    end
+	# evaluate the proc in the context of the instance
+	# for syntactic sugar :)
+	def draw(&proc)
+	end
 
-    # make each of these methods that
-    # when called add route
-    [:get, :post, :put, :delete].each do |http_method|
-    end
+	# make each of these methods that
+	# when called add route
+	[:get, :post, :put, :delete].each do |http_method|
+		define_method(http_method) do |pattern, controller_class, action_name|
+			add_route(pattern, http_method, controller_class, action_name)
+		end
+	end
 
-    # should return the route that matches this request
-    def match(req)
-    end
+	# should return the route that matches this request
+	def match(req)
+		match = []
+		@routes.each {|route| match << route if route.matches?(req)}
+		match.empty? ? nil : match.first
+	end
 
-    # either throw 404 or call run on a matched route
-    def run(req, res)
-    end
+	# either throw 404 or call run on a matched route
+	def run(req, res)
+
+	end
   end
 end
